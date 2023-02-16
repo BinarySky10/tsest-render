aa446972ea13a437dddc2662a66ece05ace855d3
sunmengmeng <sunmengmeng@bdideal.com>
2023-02-16 16:05:29 +0800

    绘制插值三角形

--- lib/primitive/Vertex.ts
+++ lib/primitive/Vertex.ts
@@ -1,10 +1,19 @@
 import { Vector3 } from "../math/Vector3";
 import { Vector4 } from "../math/Vector4";
 export default class Vertex{
-  private position: Vector3
-  private color:Vector4
+  public position: Vector3
+  public color:Vector4
   constructor(position: Vector3, color:Vector4) {
     this.position = position
     this.color = color
+  }
+  clone() {
+    return new Vertex(this.position.clone(), this.color.clone())
+  }
+  static lerp(v1: Vertex, v2: Vertex, alpha: number):Vertex {
+    const tmp = new Vertex(new Vector3(), new Vector4())
+    tmp.position.lerpVectors(v1,v2,alpha)
+    tmp.color.lerpVectors(v1, v2, alpha)
+    return tmp
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ canvas.height = window.VIEW_HEIGHT;
 
 import SunGL from '../lib/SunGL'
 import { Vector2 } from '../lib/math/Vector2'
+import { Vector3 } from '../lib/math/Vector3'
+import { Vector4 } from '../lib/math/Vector4'
+import Vertex  from '../lib/primitive/Vertex'
 import Line from '../lib/primitive/Line'
 import Triangle from '../lib/primitive/Triangle'
 const gl = new SunGL(canvas);
@@ -17,8 +20,20 @@ const gl = new SunGL(canvas);
 // //画线
 // let line = new Line(new Vector2(1,  1), new Vector2(400,  700) )
 // line.draw(gl)
+const v1 = new Vertex(
+  new Vector3(1, 1, 0),
+  new Vector4(255,0,0,255)
+)
+const v2 = new Vertex(
+  new Vector3(400, 700, 0),
+  new Vector4(0,255,0,255)
+)
+const v3 = new Vertex(
+  new Vector3(700, 500, 0),
+  new Vector4(0,0,255,255)
+)
 //画三角形框
-let triangle = new Triangle(new Vector2(1,  1), new Vector2(400,  700) , new Vector2(700,  500) )
+let triangle = new Triangle(v1, v2, v3)
 
 triangle.draw(gl)
 

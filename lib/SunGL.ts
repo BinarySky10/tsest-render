83f344832482202a97727c2277b6359e3b63d2a3
BinarySky10 <yilingxiankong@hotmail.com>
2023-03-16 17:19:33 +0800

    彩色立方体数据完成 深度测试还有问题

--- lib/SunGL.ts
+++ lib/SunGL.ts
@@ -1,73 +1,76 @@
-import { Vector4 } from "./math/Vector4"
+import { Vector4 } from "./math/Vector4";
 
-export default class SunGL{
-  private canvas: HTMLCanvasElement
-  private context: CanvasRenderingContext2D
-  private colorbuffer: ImageData
-  private depthbuffer: ImageData
-  public depthTest: boolean
-  public depthMask:boolean
+export default class SunGL {
+  private canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D;
+  private colorbuffer: ImageData;
+  private depthbuffer: ImageData;
+  public depthTest: boolean;
+  public depthMask: boolean;
   // private buffer: Uint8ClampedArray
   constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas
+    this.canvas = canvas;
     this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
     this.colorbuffer = this.context.getImageData(0, 0, 800, 800);
     this.depthbuffer = this.context.getImageData(0, 0, 800, 800);
-    this.depthTest = true
-    this.depthMask = true
-    this.clearDepth()
+    this.depthTest = true;
+    this.depthMask = true;
+    this.clearDepth();
   }
   clear() {
-    const data = this.colorbuffer.data
-    for (var i=0;i< data.length; i++) {
-        data[0+4*i]=Math.floor(0)
-        data[1+4*i]=Math.floor(0)
-        data[2+4*i]=Math.floor(0)
-        data[3+4*i]=Math.floor(0)
+    const data = this.colorbuffer.data;
+    for (var i = 0; i < data.length; i++) {
+      data[0 + 4 * i] = Math.floor(0);
+      data[1 + 4 * i] = Math.floor(0);
+      data[2 + 4 * i] = Math.floor(0);
+      data[3 + 4 * i] = Math.floor(0);
     }
-    this.context.putImageData(this.colorbuffer,0,0)
+    this.context.putImageData(this.colorbuffer, 0, 0);
   }
   clearDepth() {
-    
-    const data = this.depthbuffer.data
+    const data = this.depthbuffer.data;
     for (var i = 0; i < data.length; i++) {
-        //暂时用数字255代表深度的初始化
-        data[0+4*i]= 255 
-        data[1+4*i]= 255
-        data[2+4*i]= 255
-        data[3+4*i]= 255
+      //暂时用数字255代表深度的初始化
+      data[0 + 4 * i] = 255;
+      data[1 + 4 * i] = 255;
+      data[2 + 4 * i] = 255;
+      data[3 + 4 * i] = 255;
     }
   }
-  setPixel(x: number, y: number, z:number, color:Vector4) {
-    x = parseInt((x).toFixed())
-    y = parseInt((y).toFixed())
-    const xs = this.colorbuffer.width//800
-    const ys = this.colorbuffer.height//400
+  setPixel(x: number, y: number, z: number, color: Vector4) {
+    x = parseInt(x.toFixed());
+    y = parseInt(y.toFixed());
+    const xs = this.colorbuffer.width; //800
+    const ys = this.colorbuffer.height; //400
 
     // //深度缓冲
     if (this.depthTest) {
       //开启深度测试
-      if (z < this.depthbuffer.data[y*ys*4 + 4*x + 0 ] && this.depthbuffer.data[y*ys*4 + 4*x + 0 ]!== 255) {//深度小于已经写入深度 测试不通过
-        console.log('深度测试生效')
-        return 
+      if (
+        z < this.depthbuffer.data[y * ys * 4 + 4 * x + 0] &&
+        this.depthbuffer.data[y * ys * 4 + 4 * x + 0] !== 255
+      ) {
+        //深度小于已经写入深度 测试不通过
+        // console.log('深度测试生效')
+        return;
       }
-      
+
       //开启深度写入
       if (this.depthMask) {
-        this.depthbuffer.data[y*ys*4 + 4*x + 0 ] = z
+        this.depthbuffer.data[y * ys * 4 + 4 * x + 0] = z;
         // this.depthbuffer.data[y*ys*4 + 4*x + 1 ] = z
         // this.depthbuffer.data[y*ys*4 + 4*x + 2 ] = z
-        // this.depthbuffer.data[y*ys*4 + 4*x + 3 ] = z 
+        // this.depthbuffer.data[y*ys*4 + 4*x + 3 ] = z
       }
     }
 
     //颜色缓冲
-    this.colorbuffer.data[y*ys*4 + 4*x + 0 ] = color.x
-    this.colorbuffer.data[y*ys*4 + 4*x + 1 ] = color.y
-    this.colorbuffer.data[y*ys*4 + 4*x + 2 ] = color.z
-    this.colorbuffer.data[y*ys*4 + 4*x + 3 ] = color.w
+    this.colorbuffer.data[y * ys * 4 + 4 * x + 0] = color.x;
+    this.colorbuffer.data[y * ys * 4 + 4 * x + 1] = color.y;
+    this.colorbuffer.data[y * ys * 4 + 4 * x + 2] = color.z;
+    this.colorbuffer.data[y * ys * 4 + 4 * x + 3] = color.w;
   }
-  
+
   // setAll() {
   //   for (let y = 0; y < 400; y++) {
   //     for (let x = 0; x < 400; x++) {
@@ -76,6 +79,6 @@ export default class SunGL{
   //   }
   // }
   draw() {
-    this.context.putImageData(this.colorbuffer,0,0)
+    this.context.putImageData(this.colorbuffer, 0, 0);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
-
-import './style.css'
+import "./style.css";
 window.VIEW_WIDTH = 800;
 window.VIEW_HEIGHT = 800;
-const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+const canvas: HTMLCanvasElement = document.getElementById(
+  "canvas"
+) as HTMLCanvasElement;
 canvas.width = window.VIEW_WIDTH;
 canvas.height = window.VIEW_HEIGHT;
-import type { RenderObject } from '../lib/types'
+import type { RenderObject } from "../lib/types";
 
-import SunGL from '../lib/SunGL'
-import { Vector2 } from '../lib/math/Vector2'
-import { Vector3 } from '../lib/math/Vector3'
-import { Vector4 } from '../lib/math/Vector4'
-import Vertex from '../lib/primitive/Vertex'
-import Line from '../lib/primitive/Line'
-import Triangle from '../lib/primitive/Triangle'
-import Scene from '../lib/Scene'
-import { OrthCamera } from '../lib/Cameras/camera'
-import Renderer from '../lib/Renderer'
+import SunGL from "../lib/SunGL";
+import { Vector2 } from "../lib/math/Vector2";
+import { Vector3 } from "../lib/math/Vector3";
+import { Vector4 } from "../lib/math/Vector4";
+import Vertex from "../lib/primitive/Vertex";
+import Line from "../lib/primitive/Line";
+import Triangle from "../lib/primitive/Triangle";
+import Scene from "../lib/Scene";
+import { OrthCamera } from "../lib/Cameras/camera";
+import Renderer from "../lib/Renderer";
 
 const gl = new SunGL(canvas);
 // gl.setAll()
@@ -25,8 +26,6 @@ const gl = new SunGL(canvas);
 // //画线
 // let line = new Line(new Vector2(1,  1), new Vector2(400,  700) )
 // line.draw(gl)
-
-
 
 // {
 //   const v1 = new Vertex(
@@ -47,161 +46,118 @@ const gl = new SunGL(canvas);
 //   triangle.draw(gl)
 // }
 
-const scene = new Scene()
+const scene = new Scene();
 
-// var vertexData = [
-//   1.0, 1.0, 1.0,
-//   -1.0, 1.0, 1.0,
-//   -1.0, -1.0, 1.0,
-//   1.0, -1.0, 1.0, //front面 v0-4
-//   1.0, 1.0, 1.0,
-//   1.0, -1.0, 1.0,
-//   1.0, -1.0, -1.0,
-//   1.0, 1.0, -1.0, //right v0345
-//   1.0, 1.0, 1.0,
-//   1.0, 1.0, -1.0,
-//   -1.0, 1.0, -1.0,
-//   -1.0, 1.0, 1.0, //up v0561
-//   -1.0, 1.0, 1.0,
-//   -1.0, -1.0, 1.0,
-//   -1.0, -1.0, -1.0,
-//   -1.0, 1.0, -1.0, //left
-//   -1.0, -1.0, 1.0,
-//   1.0, -1.0, 1.0,
-//   1.0, -1.0, -1.0,
-//   -1.0, -1.0, -1.0, //down
-//   1.0, -1.0, -1.0,
-//   1.0, 1.0, -1.0,
-//   -1.0, 1.0, -1.0,
-//   -1.0, -1.0, -1.0 //back
-// ];
-// var colorData = [
-//   0.4, 0.4, 1.0,
-//   0.4, 0.4, 1.0,
-//   0.4, 0.4, 1.0,
-//   0.4, 0.4, 1.0, //front
-//   0.4, 1.0, 0.4,
-//   0.4, 1.0, 0.4,
-//   0.4, 1.0, 0.4,
-//   0.4, 1.0, 0.4, //right
-//   1.0, 0.4, 0.4,
-//   1.0, 0.4, 0.4,
-//   1.0, 0.4, 0.4,
-//   1.0, 0.4, 0.4, //up
-//   1.0, 1.0, 0.4,
-//   1.0, 1.0, 0.4,
-//   1.0, 1.0, 0.4,
-//   1.0, 1.0, 0.4, //left
-//   1.0, 0.4, 1.0,
-//   1.0, 0.4, 1.0,
-//   1.0, 0.4, 1.0,
-//   1.0, 0.4, 1.0, //btm
-//   0.4, 1.0, 1.0,
-//   0.4, 1.0, 1.0,
-//   0.4, 1.0, 1.0,
-//   0.4, 1.0, 1.0 //back
-// ]
-
+var vertexData = [
+  100.0, 100.0, 100.0, -100.0, 100.0, 100.0, -100.0, -100.0, 100.0, 100.0,
+  -100.0, 100.0, 100.0, -100.0, -100.0, 100.0, 100.0, -100.0, -100.0, 100.0,
+  -100.0, -100.0, -100.0, -100.0,
+];
+var colorData = [
+  255.0, 255.0, 255.0, 255.0, 0.0, 255.0, 255.0, 0.0, 0.0, 255.0, 255.0, 0.0,
+  0.0, 255.0, 0, 0.0, 255.0, 255.0, 0.0, 0.0, 255.0, 0.0, 0.0, 0.0,
+];
+// Indices of the vertices
 // var indicesData = [
 //   [0, 1, 2],
-//   [0, 2, 3],
-//   [4, 5, 6],
-//   [4, 6, 7],
-//   [8, 9, 10],
-//   [8, 10, 11],
-//   [12, 13, 14],
-//   [12, 14, 15],
-//   [16, 17, 18],
-//   [16, 18, 19],
-//   [20, 21, 22],
-//   [20, 22, 23]
+//   // [0, 2, 3], // front
+//   // [0, 3, 4],
+//   // [0, 4, 5], // right
+//   // [0, 5, 6],
+//   // [0, 6, 1], // up
+//   // [1, 6, 7],
+//   [1, 7, 2], // left
+//   // [7, 4, 3],
+//   // [7, 3, 2], // down
+//   // [4, 7, 6],
+//   // [4, 6, 5], // back
 // ];
-// const theVertexData = vertexData.map(item => {
-//   return item * 100.0
-// })
-// const theColorData = colorData.map(item => {
-//   return item * 255.0
-// })
-// const aBox: RenderObject = {
-//   vertexes: [],
-//   indices: []
-// }
-// for (let i = 0; i < theVertexData.length / 3.0; i++) {
-//   // i * 3
-//   // i * 3 + 1
-//   // i * 3 + 2
-//   const position = new Vector4(
-//     theVertexData[i * 3],
-//     theVertexData[i * 3 + 1],
-//     theVertexData[i * 3 + 2],
-//     1
-//   )
-//   const color = new Vector4(
-//     theColorData[i * 3],
-//     theColorData[i * 3 + 1],
-//     theColorData[i * 3 + 2],
-//     255
-//   )
-//   aBox.vertexes.push(new Vertex(
-//     position,
-//     color
-//   ))
-// }
-// aBox.indices.push(...indicesData)
-// scene.add(aBox)
-
-const v1 = new Vertex(
-  new Vector4(0, 0, 0, 1),
-  new Vector4(255, 0, 0, 255)
-)
-const v2 = new Vertex(
-  new Vector4(0, 200, 0, 1),
-  new Vector4(0, 255, 0, 255)
-)
-const v3 = new Vertex(
-  new Vector4(200, 0, 0, 1),
-  new Vector4(0, 0, 255, 255)
-)
-
-const v4 = new Vertex(
-  new Vector4(200, 200, -100, 1),
-  new Vector4(128, 0, 0, 255)
-)
-const v5 = new Vertex(
-  new Vector4(100, -200, -100, 1),
-  new Vector4(128, 0, 0, 255)
-)
-const v6 = new Vertex(
-  new Vector4(200, -100, -100, 1),
-  new Vector4(128, 0, 0, 255)
-)
-const aTriangle: RenderObject = {
-  vertexes: [
-    v1,
-    v2,
-    v3,
-    v4,
-    v5,
-    v6,
-  ],
-  indices: [[0, 1, 2], [3, 4, 5]]
+var indicesData = [
+  [1, 7, 2],
+  [0, 1, 2],
+];
+const aBox: RenderObject = {
+  vertexes: [],
+  indices: [],
+};
+for (let i = 0; i < vertexData.length / 3.0; i++) {
+  // i * 3
+  // i * 3 + 1
+  // i * 3 + 2
+  const position = new Vector4(
+    vertexData[i * 3],
+    vertexData[i * 3 + 1],
+    vertexData[i * 3 + 2],
+    1
+  );
+  const color = new Vector4(
+    colorData[i * 3],
+    colorData[i * 3 + 1],
+    colorData[i * 3 + 2],
+    255
+  );
+  aBox.vertexes.push(new Vertex(position, color));
 }
-scene.add(aTriangle)
 
+aBox.indices.push(...indicesData);
+scene.add(aBox);
+
+// const v1 = new Vertex(
+//   new Vector4(0, 0, 0, 1),
+//   new Vector4(255, 0, 0, 255)
+// )
+// const v2 = new Vertex(
+//   new Vector4(0, 200, 0, 1),
+//   new Vector4(0, 255, 0, 255)
+// )
+// const v3 = new Vertex(
+//   new Vector4(200, 0, 0, 1),
+//   new Vector4(0, 0, 255, 255)
+// )
+
+// const v4 = new Vertex(
+//   new Vector4(200, 200, -100, 1),
+//   new Vector4(128, 0, 0, 255)
+// )
+// const v5 = new Vertex(
+//   new Vector4(100, -200, -100, 1),
+//   new Vector4(128, 0, 0, 255)
+// )
+// const v6 = new Vertex(
+//   new Vector4(200, -100, -100, 1),
+//   new Vector4(128, 0, 0, 255)
+// )
+// const aTriangle: RenderObject = {
+//   vertexes: [
+//     v1,
+//     v2,
+//     v3,
+//     v4,
+//     v5,
+//     v6,
+//   ],
+//   indices: [[0, 1, 2], [3, 4, 5]]
+// }
+// scene.add(aTriangle)
+
+// const camera = new OrthCamera(
+//   new Vector3(0, 0, 1000),
+//   new Vector3(0, 0, -1),
+// )
 const camera = new OrthCamera(
-  new Vector3(0, 0, 10000),
-  new Vector3(0, 0, -1),
-)
-const renderer = new Renderer(scene, camera, gl)
+  new Vector3(250, 250, 250),
+  new Vector3(-1, -1, -1)
+);
+const renderer = new Renderer(scene, camera, gl);
 
-window.angle = 0
-let d = 0.01
+window.angle = 0;
+let d = 0.01;
 function loop() {
-  renderer.render()
+  renderer.render();
   // window.angle+=d
   requestAnimationFrame(() => {
-    loop()
-  })
+    loop();
+  });
 }
 
-loop()
+loop();
